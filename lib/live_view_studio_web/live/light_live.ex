37e50c9231065955ce97b127c1c61b583ec0ec60
kamilpikula <pikusix1@o2.pl>
2020-07-23 17:56:24 +0200

    initial light feature

---
+++ lib/live_view_studio_web/live/light_live.ex
@@ -0,0 +1,15 @@
+defmodule LiveViewStudioWeb.LightLive do
+  use LiveViewStudioWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket, :brightness, 10)
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~L"""
+    <h1>Front Light</h1>
+    <%= @brightness %>
+    """
+  end
+end
