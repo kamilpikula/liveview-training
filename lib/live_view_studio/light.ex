129928bc22ea3eaa140b1c78104ff4ba3423885b
kamilpikula <pikusix1@o2.pl>
2020-08-06 23:22:00 +0200

    change color of meter

---
+++ lib/live_view_studio/light.ex
@@ -0,0 +1,5 @@
+defmodule LiveViewStudio.Light do
+  def temp_color(3000), do: "#948d29"
+  def temp_color(4000), do: "#ccc239"
+  def temp_color(5000), do: "#f5e942"
+end
--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -1,8 +1,12 @@
 defmodule LiveViewStudioWeb.LightLive do
   use LiveViewStudioWeb, :live_view
 
+  alias LiveViewStudio.Light
+
   def mount(_params, _session, socket) do
-    socket = assign(socket, :brightness, 10)
+    socket = assign(socket,
+      brightness: 10,
+      temp: 3000)
     {:ok, socket}
   end
 
@@ -39,12 +43,19 @@ defmodule LiveViewStudioWeb.LightLive do
     {:noreply, socket}
   end
 
+  def handle_event("change-temp", %{"temp" => temp}, socket) do
+    temp = String.to_integer(temp)
+    socket = assign(socket, temp: temp)
+    {:noreply, socket}
+  end
+
   def render(assigns) do
     ~L"""
     <h1>Front Light</h1>
     <div id="light">
       <div class="meter">
-        <span style="width: <%= @brightness %>%">
+        <span style="width: <%= @brightness %>%;
+                     background-color: <%= Light.temp_color(@temp)%>">
           <%= @brightness %>%
         </span>
       </div>
@@ -73,6 +84,23 @@ defmodule LiveViewStudioWeb.LightLive do
         <input type="range" min="0" max="100"
               name="brightness" value="<%= @brightness %>" />
       </form>
+
+      <form phx-change="change-temp">
+        <h2>Change color of meter</h2>
+
+        <input type="radio" id="3000" name="temp" value="3000"
+          <%= if 3000 == @temp, do: "checked" %> />
+        <label for="3000">3000</label>
+
+        <input type="radio" id="4000" name="temp" value="4000"
+          <%= if 4000 == @temp, do: "checked" %> />
+        <label for="4000">4000</label>
+
+        <input type="radio" id="5000" name="temp" value="5000"
+          <%= if 5000 == @temp, do: "checked" %> />
+        <label for="5000">5000</label>
+      </form>
+
     </div>
     """
   end
